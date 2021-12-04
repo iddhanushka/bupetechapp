8303b5a7d59ced3730953cef219cc9a960ac5c55
Damith Weerawardana <damith@bistecglobal.com>
2021-12-04 06:49:11 +0530

    Add toast and implement with the contact form submit

--- src/main.js
+++ src/main.js
@@ -9,10 +9,11 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 import "normalize.css"
 import i18n from './i18n'
 import 'vue-toast-notification/dist/theme-default.css';
-
+import Toaster from '@meforma/vue-toaster';
 
 const app = createApp(App).use(i18n);
 app.use(router);
 app.use(BootstrapVue3);
 app.mount('#app');
+app.use(Toaster);
 
