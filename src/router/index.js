80a9839dd19a02367cd7682aba97b71dd4b8b56e
Damith Weerawardana <damith@bistecglobal.com>
2021-12-05 20:02:56 +0530

    Added Help content page

--- src/router/index.js
+++ src/router/index.js
@@ -110,6 +110,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/PrivarcyTerms.vue')
+  },
+  {
+    path: '/help',
+    name: 'Help',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/HelpContent.vue')
   }
   
 
