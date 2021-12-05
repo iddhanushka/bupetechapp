fa7041a616fb41c9f48541df95d6a4cf4d4136e9
dilan <dilan@cleato.com>
2021-12-05 19:28:56 +0530

    privarcy page

--- src/router/index.js
+++ src/router/index.js
@@ -102,6 +102,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Modules.vue')
+  },
+  {
+    path: '/privarcy-terms',
+    name: 'PrivarcyTerms',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/PrivarcyTerms.vue')
   }
   
 
